c2ca3ba7d260da7e302fab35147821a7605057b5
gowgow1 <gow_gow1@mail.ru>
2021-02-27 00:45:22 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (matrix=[]) {
+    let array = [];
+    if (matrix.length > 0){
+            array = matrix.map((item, index) => (index==0 || index%2==0)? item.sort((a,b) => a-b ): item.sort((a,b) => b-a));
+            let f = []
+            return f.concat(...array)
+    }
+    return array
+  }
